346daa80ac76d0150d5dcb525e8c245e9c2dc356
aceofspade96 <ger.shishir@gmail.com>
2025-08-18 15:42:55 +1000

    changes on home page

--- app/homepage.tsx
+++ app/homepage.tsx
@@ -4,12 +4,9 @@ import HamburgerMenu from './Components/HamburgerMenu';
 export default function Home() {
   return (
     <div>
+      
+      <h1> Student ID: 22239026 </h1>
       <HamburgerMenu />
-      <h1>Welcome to My Next.js App 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, harum illum officiis 
-        laudantium eveniet adipisci, pariatur iste quidem vel quam officia suscipit accusantium commodi
-         ullam eum ab quae rerum repellat.
-      </h1>
     </div>
   );
 }
