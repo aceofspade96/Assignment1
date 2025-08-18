df7149009bbbadce07182acb2f39412fd64e9f00
aceofspade96 <ger.shishir@gmail.com>
2025-08-18 14:58:16 +1000

    Modifed-page

--- app/Components/HamburgerMenu.tsx
+++ app/Components/HamburgerMenu.tsx
@@ -21,8 +21,9 @@ const HamburgerMenu = () => {
         <ul>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="/tests">Tests</a></li>
+          <li><a href="/prisma">Prisma/Sequeelize</a></li>
+          <li><a href="/docker">Docker</a></li>
         </ul>
       </nav>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,11 @@ export default function Home() {
   return (
     <div>
       <HamburgerMenu />
-      <h1>Welcome to My Next.js App</h1>
+      <h1>Welcome to My Next.js App 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, harum illum officiis 
+        laudantium eveniet adipisci, pariatur iste quidem vel quam officia suscipit accusantium commodi
+         ullam eum ab quae rerum repellat.
+      </h1>
     </div>
   );
 }
